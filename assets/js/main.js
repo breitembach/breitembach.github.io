a99f2d1b9796b65e1384ec217e396c6b111f313e
breitembach <lucas.breitembach@hotmail.com>
2017-07-17 23:08:52 -0300

    website

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+element = ".effect-16";
+   $(element).focusout(function(){
+      if($(this).val() != ""){
+         $(this).addClass("has-content");
+      }else{
+         $(this).removeClass("has-content");
+      }
+   });
