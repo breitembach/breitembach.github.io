1a4441f630df0342e7f12f7561bbe4eb57b79d01
breitembach <lucas.breitembach@hotmail.com>
2017-07-18 23:56:41 -0300

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
+$( document ).ready(function() {
 
-element = ".effect-16";
+   element = ".effect-16";
    $(element).focusout(function(){
       if($(this).val() != ""){
          $(this).addClass("has-content");
@@ -7,3 +8,15 @@ element = ".effect-16";
          $(this).removeClass("has-content");
       }
    });
+
+   pos = 0;
+   $(".aside-menu-list").click(function(){
+      $("body").animate({scrollTop:pos},500);
+   });
+   
+   $(function () {
+      $('[data-toggle="tooltip"]').tooltip();
+   });
+
+
+});
