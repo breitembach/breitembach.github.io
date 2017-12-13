7e6c5a7db446fac5a7f4095545eb8a8fd7a228f9
breitembach <lucas.breitembach@hotmail.com>
2017-12-12 22:33:47 -0200

    update works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+   //input contact
    element = ".effect-16";
    $(element).focusout(function(){
       if($(this).val() != ""){
@@ -9,16 +10,17 @@ $( document ).ready(function() {
       }
    });
 
+   //MENU
    pos = 0;
    $(".aside-menu-list").click(function(){
       $("body").animate({scrollTop:pos},500);
    });
-   
+   //Tooltip
    $(function () {
       $('[data-toggle="tooltip"]').tooltip();
    });
 
-   
+   //Contacts
    $(".btn-contact").click(function(e) {
       email = document.getElementById("email").value;
       message = document.getElementById("message").value;
@@ -29,5 +31,65 @@ $( document ).ready(function() {
       }
       
    });
+
+   var cont ;
+   var init = $(".timeline-item").length;
+   
+   $("#projects .active span").html(init);
+ 
+   // FILTER Categories
+   $(".filter-button").click(function(){
+     
+      $('.filter-button').removeClass("active");
+      
+      $(this).addClass("active");
+         var value = $(this).attr('data-filter');
+         if(value == "Todos") {
+            $('.filter').show('1000');
+         }
+         else {
+            $(".filter").not('.'+value).hide('3000');
+           
+            $('.filter').filter('.'+value).show('3000');
+         }
+
+
+         $("#projects .amount").html("");
+
+         switch ($(this).data("filter")) {
+
+            case "all":
+            var all = $(".all").length | 0;
+            $("#projects .active span").html(all);
+            break;
+            
+            case "web":
+               var web = $(".web").length;
+               $("#projects .active span").html(web);
+              break;
+
+            case "better":
+               var bet = $(".better").length;
+               $("#projects .active span").html(bet);
+               break;
+
+            case "mobile":
+               var mobile = $(".mobile").length;
+               $("#projects .active span").html(mobile);
+               break;
+
+            case "desktop":
+               var desktop = $(".desktop").length ;
+               $("#projects .active span").html(desktop);
+               break;
+
+           default:
+           
+              break;
+        }
+         
+  });
+
+ 
    
 });
